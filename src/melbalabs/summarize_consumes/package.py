cb34e85e22d18dd230efd39e1f4a98a06bd079c5
Creohex <paradise.source@gmail.com>
2024-05-17 17:54:37 +0300

    bump version

--- src/melbalabs/summarize_consumes/package.py
+++ src/melbalabs/summarize_consumes/package.py
@@ -1,6 +1,6 @@
 import importlib.metadata
 
-VERSION = "2024.1062-c"
+VERSION = "2024.1062-d"
 PROJECT_NAME = __package__
 PROJECT_METADATA = importlib.metadata.metadata(PROJECT_NAME)
 PROJECT_URL = PROJECT_METADATA['Project-URL']
